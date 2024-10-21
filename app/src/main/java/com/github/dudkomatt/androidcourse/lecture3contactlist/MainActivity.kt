5ebdcc8b3fd7feaa31989e58e460e9d9828429b5
DudkoMatt <26856709+DudkoMatt@users.noreply.github.com>
2024-10-21 21:24:41 +0300

    init: Project template

---
+++ app/src/main/java/com/github/dudkomatt/androidcourse/lecture3contactlist/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.github.dudkomatt.androidcourse.lecture3contactlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.dudkomatt.androidcourse.lecture3contactlist.ui.theme.Lecture3ContactListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lecture3ContactListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lecture3ContactListTheme {
+        Greeting("Android")
+    }
+}
